f11566b667a731b038755b3b947e78ac45937f15
KurohanaJuri <chau-ying-kot@hotmail.com>
2020-03-05 12:31:44 +0100

    Refactoring, move equals from Dolor to Money

--- src/Dollar.java
+++ src/Dollar.java
@@ -7,9 +7,4 @@ public class Dollar extends Money {
     Dollar times(int multiplier) {
         return new Dollar(amount * multiplier);
     }
-
-    public boolean equals(Object object) {
-        Money money = (Dollar) object;
-        return amount == money.amount;
-    }
 }
--- src/Money.java
+++ src/Money.java
@@ -2,4 +2,8 @@ public class Money{
 
     protected int amount;
 
+    public boolean equals(Object object) {
+        Money money = (Dollar) object;
+        return amount == money.amount;
+    }
 }
