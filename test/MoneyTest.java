26be63ec52c136c894866b45407db1a5d7c5b03a
KurohanaJuri <chau-ying-kot@hotmail.com>
2020-03-05 12:24:51 +0100

    p30 fixes #10

--- test/MoneyTest.java
+++ test/MoneyTest.java
@@ -12,6 +12,13 @@ class MoneyTest {
     }
 
     @Test
+    public void testFrancMultiplication() {
+        Franc five = new Franc(5);
+        assertEquals(new Franc(10), five.times(2));
+        assertEquals(new Franc(15), five.times(3));
+    }
+
+    @Test
     public void testEquality(){
         assertTrue(new Dollar(5).equals(new Dollar(5)));
         assertFalse(new Dollar(5).equals(new Dollar(6)));
