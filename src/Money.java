235827af395be4b31a88461f4e61ee38af1a0d93
KurohanaJuri <chau-ying-kot@hotmail.com>
2020-03-05 12:39:58 +0100

    p34 remove equals in Franc and fixe equals in Money

--- src/Money.java
+++ src/Money.java
@@ -3,7 +3,7 @@ public class Money{
     protected int amount;
 
     public boolean equals(Object object) {
-        Money money = (Dollar) object;
+        Money money = (Money) object;
         return amount == money.amount;
     }
 }
