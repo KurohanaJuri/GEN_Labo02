284dd64c4b76d6e1c00ca65d85ca748df2c10065
Stephane Bottin <stephane.bottin@heig-vd.ch>
2020-03-12 11:03:56 +0100

    Added currency to subclasses

--- src/Dollar.java
+++ src/Dollar.java
@@ -1,10 +1,18 @@
 public class Dollar extends Money {
 
+    private String currency;
+
     Dollar(int amount) {
         this.amount = amount;
+        currency = "USD";
     }
 
     Money times(int multiplier) {
         return new Dollar(amount * multiplier);
+    }
+
+    @Override
+    String currency() {
+        return currency;
     }
 }
--- src/Franc.java
+++ src/Franc.java
@@ -1,11 +1,18 @@
 public class Franc extends Money{
 
+    private String currency;
 
     Franc(int amount) {
         this.amount = amount;
+        currency = "CHF";
     }
 
     Money times(int multiplier) {
         return new Franc(amount * multiplier);
+    }
+
+    @Override
+    String currency() {
+        return currency;
     }
 }
