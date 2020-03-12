e230ec84d3fd526d6724d85771e233566e6e5691
Stephane Bottin <stephane.bottin@heig-vd.ch>
2020-03-12 11:09:51 +0100

    Currency refactoring to super class

--- test/MoneyTest.java
+++ test/MoneyTest.java
@@ -7,8 +7,8 @@ class MoneyTest {
     @Test
     public void testMultiplication() {
         Money five = Money.dollar(5);
-        assertEquals(new Dollar(10), five.times(2));
-        assertEquals(new Dollar(15), five.times(3));
+        assertEquals(Money.dollar(10), five.times(2));
+        assertEquals(Money.dollar(15), five.times(3));
     }
 
     @Test
